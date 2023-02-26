03839af69460394ba2e88c86d344d9c7c73b066d
Wardatunnisa <Wardatunnisa2001@gmail.com>
2023-02-26 22:46:09 +0700

    tugas run test

--- cypress/e2e/step_definitions/login.js
+++ cypress/e2e/step_definitions/login.js
@@ -1,21 +1,18 @@
+import LoginPage from "../run_test/login.test";
 import {
   Given,
   When,
   Then,
 } from "@badeball/cypress-cucumber-preprocessor";
 
-
 Given("I open login page", () => {
-  cy.visit("http://zero.webappsecurity.com/login.html");
+  LoginPage.visit();
 });
 
 When("I submit login", () => {
-    cy.get('#user_login').type('username')
-    cy.get('#user_password').type('password')
-
-    cy.get('#user_remember_me').click()
-
-    cy.get('input[name="submit"]').click()
+  LoginPage.fillUsername('username');
+  LoginPage.fillPassword('password');
+  LoginPage.signIn();
 });
 
 Then("I should see homepage", () => {
